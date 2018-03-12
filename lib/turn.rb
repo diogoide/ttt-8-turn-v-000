ca0247fd6399a45a09dbcb07a1b2fcff2d378422
Diogo Ide <diogoide@gmail.com>
2018-03-12 00:02:12 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,6 +27,7 @@ def valid_move?(board,index)
     gets input_to_index(board)
     if valid_move?(board,index) return true
       return "X"
-    else puts "Please enter 1-9:"
+    else 
+      puts "Please enter 1-9:"
   end
 end
